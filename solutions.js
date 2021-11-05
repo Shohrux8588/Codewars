9dc0e4aa7528d982a52a3c2cddd46d4eef170da4
Shohrux8588 <70914015+Shohrux8588@users.noreply.github.com>
2021-11-05 16:32:33 +0500

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,1 +1,8 @@
-// the first solution
+// Elevator Distance
+function elevatorDistance(array) {
+  let distance = 0;
+  for(let i = 0; i < array.length - 1; i++){
+    distance += Math.abs(array[i]-array[i + 1]);
+  }
+  return distance;
+}
